3471e3c33f977b24e960ea33ca826126e5e16717
cgansen <cgansen@gmail.com>
2011-12-04 15:33:31 -0600

    Flagging refs to previous impl. in Boston

--- test/functional/main_controller_test.rb
+++ test/functional/main_controller_test.rb
@@ -5,7 +5,7 @@ class MainControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_select 'title', 'Adopt a Hydrant'
-    assert_select 'p#tagline', 'Claim responsibility for shoveling out a fire hydrant after it snows.'
+    assert_select 'p#tagline', 'Claim responsibility for shoveling out a fire hydrant after it snows.'  # FIXME
     assert_select 'form' do
       assert_select '[action=?]', '/'
       assert_select '[method=?]', 'post'
