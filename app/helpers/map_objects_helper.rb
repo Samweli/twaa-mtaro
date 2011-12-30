a803927ea8996f7ab218edcb6272341ef7ed4ab8
Kagan Unaldi <kunaldi@gmail.com>
2011-12-30 16:07:10 -0600

    kml bug test

--- app/helpers/map_objects_helper.rb
+++ app/helpers/map_objects_helper.rb
@@ -7,13 +7,18 @@ module MapObjectsHelper
     value ? 'yes' : 'no'
   end
   
-  def format_color(o)
-    if o.cleared
-      '#linestyle_blue'
-    elsif o.claimed
-      '#linestyle_yellow'
-    elsif o.need_help
-      '#linestyle_red'
+  def format_color(s)
+    #K: temp hack, 1-n issue make it efficient
+    if o = MapObject.find_by_gid(s.gid)
+      if o.cleared
+        '#linestyle_blue'
+      elsif o.claimed
+        '#linestyle_yellow'
+      elsif o.need_help
+        '#linestyle_red'
+      else
+        '#linestyle_white'
+      end
     else
       '#linestyle_white'
     end
