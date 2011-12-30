07cb5481dcf8140fc9d43801acbb1eb9f32d9534
Kagan Unaldi <kunaldi@gmail.com>
2011-12-30 15:48:51 -0600

    disabled auth for testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :authenticate_user!
+  #before_filter :authenticate_user!
   before_filter :set_flash_from_params
   before_filter :set_locale
 
