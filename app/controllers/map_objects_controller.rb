6fe75f9ecfc356b38a191ca9fe55db6ca72eb22d
Kagan Unaldi <kunaldi@gmail.com>
2012-01-04 15:29:55 -0600

    UI tweaks, better login reg

--- app/controllers/map_objects_controller.rb
+++ app/controllers/map_objects_controller.rb
@@ -7,32 +7,28 @@ class MapObjectsController < ApplicationController
   def create
     find_object
     
-    #if user_signed_in?
-      if @map_object.try :belongs_to_me?
-        puts 'already claimed!'
-        # trying to adopt an object that I already adopted
-      else
-        @map_object = MapObject.new do |o|
-          if sidewalk = Sidewalk.find_by_gid(@gid)
-            loc = Address.geocode("#{sidewalk.address}, Chicago, IL")
-            o.lat, o.lng = loc[0], loc[1]
-            o.object_type = MapObject::OBJECT_TYPES[:sidewalk]
-            o.source_type = MapObject::SOURCE_TYPES[:user]
-            o.source_id = current_user.id
-            o.gid = params[:id]
-            o.claimed = true
-          end
-        end
-        
-        if @map_object.save
-          respond_with @map_object
-        else
-          render(:json => {"errors" => @map_object.errors}, :status => 500)
+    if @map_object.try :belongs_to_me?
+      puts 'already claimed!'
+      # trying to adopt an object that I already adopted
+    else
+      @map_object = MapObject.new do |o|
+        if sidewalk = Sidewalk.find_by_gid(@gid)
+          loc = Address.geocode("#{sidewalk.address}, Chicago, IL")
+          o.lat, o.lng = loc[0], loc[1]
+          o.object_type = MapObject::OBJECT_TYPES[:sidewalk]
+          o.source_type = MapObject::SOURCE_TYPES[:user]
+          o.source_id = current_user.id
+          o.gid = params[:id]
+          o.claimed = true
         end
       end
-    #else
-    #  render :template => 'users/new'
-    #end
+      
+      if @map_object.save
+        respond_with @map_object
+      else
+        render(:json => {"errors" => @map_object.errors}, :status => 500)
+      end
+    end
   end
 
   def show
