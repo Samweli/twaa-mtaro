b9a8fc59cfabd52e8b0d35ee8fb33667d4b12f2b
Kagan Unaldi <kunaldi@gmail.com>
2012-01-08 11:14:10 -0600

    Re-design tables and columns

---
+++ db/migrate/00000000000002_create_sidewalk_claim.rb
@@ -0,0 +1,13 @@
+class CreateSidewalkClaims < ActiveRecord::Migration
+  def change
+    create_table :sidewalk_claims do |t|
+      t.timestamps
+      t.integer :user_id
+      t.integer :gid
+      t.string :notes
+      t.index :gid
+      t.index :user_id
+    end
+
+  end
+end
