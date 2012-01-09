c9e666dc7b6f394247551a5f3a79dbc77286b192
Kagan Unaldi <kunaldi@gmail.com>
2012-01-09 09:49:21 -0600

    Model and helper cleanups

--- app/models/reminder.rb
+++
@@ -1,6 +0,0 @@
-class Reminder < ActiveRecord::Base
-  validates_presence_of :from_user, :to_user, :map_object
-  belongs_to :from_user, :class_name => "User"
-  belongs_to :to_user, :class_name => "User"
-  belongs_to :map_object
-end
--- app/models/sidewalk.rb
+++ app/models/sidewalk.rb
@@ -2,6 +2,9 @@ class Sidewalk < ActiveRecord::Base
   set_table_name 'chicagosidewalks'
   
   has_many :claims, :class => 'sidewalk_claims', :foreign_key => "gid"
+  validates_presence_of :lat, :lon
+  
+  include Geokit::Geocoders
 
   def self.find_closest(lat, lng, limit=40, geo_buffer_size = 0.05)
     query = %Q(
@@ -24,5 +27,49 @@ class Sidewalk < ActiveRecord::Base
     
     find_by_sql([query, lng.to_f, lat.to_f, geo_buffer_size.to_f, limit.to_i])
   end
+  
+  def reverse_geocode
+    @reverse_geocode ||= MultiGeocoder.reverse_geocode([lat, lng])
+  end
+
+  def street_number
+    reverse_geocode.street_number
+  end
+
+  def street_name
+    reverse_geocode.street_name
+  end
+
+  def street_address
+    reverse_geocode.street_address
+  end
+
+  def city
+    reverse_geocode.city
+  end
+
+  def state
+    reverse_geocode.state
+  end
+
+  def zip
+    reverse_geocode.zip
+  end
+
+  def country_code
+    reverse_geocode.country_code
+  end
+
+  def country
+    reverse_geocode.country
+  end
+
+  def full_address
+    reverse_geocode.full_address
+  end
+  
+
+
+
 
 end
