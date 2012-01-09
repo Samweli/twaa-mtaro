801119814fb286002a511e4cdc51cadbb2fdb519
Kagan Unaldi <kunaldi@gmail.com>
2012-01-09 16:01:13 -0600

    Fixed counter name

--- app/helpers/sidewalks_helper.rb
+++ app/helpers/sidewalks_helper.rb
@@ -2,7 +2,7 @@ module SidewalksHelper
   def format_color(s)
     if s.cleared
       "#cleared"
-    elsif s.num_adopted > 0
+    elsif s.claims_count > 0
       "#adopted"
     elsif s.need_help
       "#needs_help"
