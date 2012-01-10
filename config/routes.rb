8a8e99b4901fbf5fbac4e26b36a4be138f96b87f
Kagan Unaldi <kunaldi@gmail.com>
2012-01-10 14:30:43 -0600

    widget tweaks, sidebar tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ AdoptAThing::Application.routes.draw do
       :sessions => 'sessions',
   }
   get 'address' => 'addresses#show', :as => 'address'
-  get 'info_window' => 'info_window#index', :as => 'info_window'
-  get 'sitemap' => 'sitemaps#index', :as => 'sitemap'
+  get 'address_report' => 'addresses#report', :as => 'address_report'
   get 'tos' => 'info_window#tos', :as => 'tos'
   resources :sidewalks
   resources :sidewalk_claims
