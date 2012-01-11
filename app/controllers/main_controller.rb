9a5c92640500adde8d3b10b0eef8bc4cc4398731
Kagan Unaldi <kunaldi@gmail.com>
2012-01-11 02:14:03 -0600

    Ajax login update

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,16 @@
 class MainController < ApplicationController
+  #respond_to :html, :json
   def index
     if user_signed_in?
       @my_sidewalks = current_user.sidewalk_claims.includes(:sidewalk).all
+    end
+
+    respond_to do |format|
+      format.html
+      format.json {
+        render :json => { :user_badge => render_to_string(:partial => 'user_badge.html.haml'),
+                          :sidewalk_list => render_to_string(:partial => 'sidewalk_list.html.haml') }
+      }
     end
   end
 end
