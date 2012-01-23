021ade4c7bd0725a79140f35cf6874749a0add5f
Kagan Unaldi <kunaldi@gmail.com>
2012-01-23 16:51:23 -0600

    Mailer base settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,10 +62,10 @@ AdoptASidewalk::Application.configure do
 end
 
 ActionMailer::Base.smtp_settings = {
-  :address        => "smtp.sendgrid.net",
+  :address        => "webappsmail.cityofchicago.org",
   :port           => "25",
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => ENV['SENDGRID_DOMAIN']
+  :user_name      => ENV['MAILER_CS_USERNAME'] || '',
+  :password       => ENV['MAILER_CS_PASSWORD'] || '',
+  :domain         => ENV['MAILER_CS_DOMAIN'] || ''
 }
