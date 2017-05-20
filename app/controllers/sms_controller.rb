6ce5e6e6999021d8bc01e543fbadc47e2f6d38b0
Samweli <smwltwesa6@gmail.com>
2017-05-20 15:26:39 +0300

    add drain info window

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,19 +1,31 @@
 class SmsController < ApplicationController
 	def new
+		from_number = params[:from]
+		drain_status = params[:body]
+
+		message = 'ujumbe'
+
+
+		sidewalk = Sidewalk.find_by_user_phone_number(from_number)
+        unless sidewalk.blank?
+        	if (drain_status == 'msafi')
+        		sidewalk.cleared = true
+        		sidewalk.need_help = false
+        		if (sidewalk.save(validate: false))
+        			message = 'Asante, kwa kusafisha mtaro.'
+        		else
+        			message = 'Kuna tatizo kupokea taarifa za mtaro,'
+        			           'Jaribu tena baadae'
+        		end
+        	end
+        else
+        	message = 'Samahani hatukuweza kutambua mtaro wako'
+      	end
 
 		twiml = Twilio::TwiML::Response.new do |response|
-	     response.Message "Asante"
+	     response.Message message
 	    end
 	    render xml: twiml.to_xml, content_type:'text/xml'
-
-		# sidewalk = Sidewalk.find_by_user_phone_number(from_number)
-
-		# if (sidewalk_status == 'msafi')
-		# 	sidewalk.cleared = true
-		# 	sidewalk.need_help = false
-
-  #     		sidewalk.save(validate: false)	
-  #     	end
 
 	end
 
