6491b06c900dd6bc215e62f56abed7760eb20710
Mloweedgar <mloweedgar0@gmail.com>
2017-08-04 15:30:13 +0300

    removed cached schema.rb file and modified logic for users to view their drains

--- db/migrate/20170801130858_change_drains.rb
+++ db/migrate/20170801130858_change_drains.rb
@@ -1,8 +1,6 @@
 class ChangeDrains < ActiveRecord::Migration
   def up
     change_table :mitaro_dar do |t|
-      t.decimal :lat, :default => nil, :precision => 16, :scale => 14
-      t.decimal :lng, :default => nil, :precision => 16, :scale => 14
       t.boolean :cleared
       t.boolean :need_help
       t.string  :address
@@ -13,8 +11,6 @@ class ChangeDrains < ActiveRecord::Migration
   end
 
   def down
-    remove_column :mitaro_dar, :lat
-    remove_column :mitaro_dar, :lng
     remove_column :mitaro_dar, :cleared
     remove_column :mitaro_dar, :need_help
     remove_column :mitaro_dar, :address
