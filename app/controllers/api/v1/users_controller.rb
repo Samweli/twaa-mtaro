24387c1249c37609a1ab43217fc8aaca5df0c9cd
Mloweedgar <mloweedgar0@gmail.com>
2017-09-06 13:26:51 +0300

    added create,update and destroy to users api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -19,4 +19,60 @@ class Api::V1::UsersController < Api::V1::BaseController
 
     render(json: Api::V1::UserSerializer.new(user).to_json)
   end
+
+  def create
+    user = User.create(first_name: params[:first_name],
+                       sms_number: params[:sms_number],
+                       last_name: params[:last_name],
+                       password: params[:password],email: params[:email],
+                       street_id: params[:street_id])
+    return api_error(status: 422, errors: user.errors) unless user.valid?
+
+    user.save!
+
+    render(
+        json: Api::V1::UserSerializer.new(user).to_json,
+        status: 201,
+        location: api_v1_user_path(user.id)
+    )
+  end
+
+  def update
+    user = User.find(params[:id])
+
+    if !user.update_attributes(first_name: params[:first_name],
+                               sms_number: params[:sms_number],
+                               last_name: params[:last_name],
+                               password: params[:password],email: params[:email],
+                               street_id: params[:street_id])
+      return api_error(status: 422, errors: user.errors)
+    end
+
+    render(
+        json: Api::V1::UserSerializer.new(user).to_json,
+        status: 200,
+        location: api_v1_user_path(user.id)
+    )
+  end
+
+  def destroy
+    user = User.find(params[:id])
+
+    if !user.destroy
+      return api_error(status: 500)
+    end
+
+    head status: 204
+  end
+
+
+  private
+
+  def create_params
+    params.require(:user)
+  end
+  def update_params
+    create_params
+  end
+
 end
--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,8 +1,6 @@
 class Api::V1::UserSerializer < Api::V1::BaseSerializer
-  attributes :email, :first_name, :last_name, :organization, :sms_number,
-             :password, :password_confirmation, :street_id, :remember_me,
-             :authentication_token
-
+  attributes :id, :email, :first_name, :last_name, :organization, :sms_number,
+             :street_id, :authentication_token
 
   def created_at
     object.created_at.in_time_zone.iso8601 if object.created_at
