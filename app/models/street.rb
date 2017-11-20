43ad4c7d90db841afd2335432fdde330c8a66984
Mloweedgar <mloweedgar0@gmail.com>
2017-11-20 17:04:57 +0300

    added Id field in returned streets

--- app/models/street.rb
+++ app/models/street.rb
@@ -1,5 +1,5 @@
 class Street < ActiveRecord::Base
-   attr_accessible :street_name, :ward_name, :municipal_name, :city_name, :lat, :lng
+   attr_accessible :street_name, :ward_name, :municipal_name, :city_name, :lat, :lng, :id
    validates_presence_of :street_name, :ward_name
    has_many :users
 end
