716591dbc71be4e101875e6c762fcad44c329558
Mloweedgar <mloweedgar0@gmail.com>
2017-11-21 16:16:46 +0300

    added function for setting drain priority to flood prone

--- app/models/priority.rb
+++ app/models/priority.rb
@@ -1,4 +1,6 @@
 class Priority < ActiveRecord::Base
   attr_accessible :name
   validates :name, presence: true, uniqueness: true
+  has_many :set_priorities
+  has_many :drains, through: :set_priorities
 end
