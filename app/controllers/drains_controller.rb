524851cd3aa34194cdaa33274396cf7e7ebd4724
Mloweedgar <mloweedgar0@gmail.com>
2017-12-07 16:40:02 +0300

    fix error in reminder api

--- app/controllers/drains_controller.rb
+++ app/controllers/drains_controller.rb
@@ -76,15 +76,13 @@ class DrainsController < ApplicationController
     user = current_user
 
     claim = drain.claims.find_by_user_id(user.id)
-    # street_leader = User.find_by_role_and_street_id(2, user.street_id)
     street_leader = User.joins(:roles).where(roles: { id: 2 })
                         .find_by_street_id(user.street_id)
-    puts "this is the leader #{street_leader.first_name}"
 
     if params.has_key?(:shoveled)
 
       status = (shoveled ? t("messages.clear_status") : t("messages.dirt_status"))
-      if !(user.has_role 2)
+      if !(user.has_role(2) )
         unless claim
           claim.update_attribute(:shoveled, shoveled)
           claim.save(validate: false)
