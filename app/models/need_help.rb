bb55d1661e644aae71b03ab30be7c48febafddf7
Mloweedgar <mloweedgar0@gmail.com>
2018-01-25 19:15:55 +0300

    adding latitude and longitude in creating need help request

--- app/models/need_help.rb
+++ app/models/need_help.rb
@@ -2,7 +2,7 @@ class NeedHelp < ActiveRecord::Base
   acts_as_paranoid
   attr_accessible :gid, :help_needed, :user_id, :need_help_category_id, :status,
                   :pending, :resolved, :pending_description,
-                  :resolved_description
+                  :resolved_description, :lat, :long
   validates_presence_of :gid, :user_id, :need_help_category_id
   belongs_to :drain, :class_name => 'Drain', :foreign_key => "gid"
   belongs_to :user
