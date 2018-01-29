ab04057575eff2d33904000dda2af9c3f8698e05
Mloweedgar <mloweedgar0@gmail.com>
2018-01-29 15:14:22 +0300

    adding ward and municipal data to users api

--- app/serializers/api/v1/street_serializer.rb
+++ app/serializers/api/v1/street_serializer.rb
@@ -1,6 +1,7 @@
 class Api::V1::StreetSerializer < Api::V1::BaseSerializer
 
-  attributes :street_name, :ward_id, :lat, :lng, :name, :label, :value
+  attributes :street_name, :ward, :lat, :lng, :name, :label, :value
+  has_one :ward, embed: :objects
 
   def name
     object.street_name
@@ -12,5 +13,8 @@ class Api::V1::StreetSerializer < Api::V1::BaseSerializer
 
   def value
     object.street_name
+  end
+  def ward
+    object.ward
   end
 end
--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,6 +1,6 @@
 class Api::V1::UserSerializer < Api::V1::BaseSerializer
   attributes :id, :email, :first_name, :last_name, :organization, :sms_number,
-             :role_requested
+             :role_requested, :street, :ward, :municipal, :roles
 
   has_many :roles, through: :assignments, embed: :objects
   has_one :street, embed: :objects
@@ -12,5 +12,20 @@ class Api::V1::UserSerializer < Api::V1::BaseSerializer
 
   def updated_at
     object.updated_at.in_time_zone.iso8601 if object.created_at
+  end
+
+  def street
+    object.street
+  end
+  def ward
+    object.street.ward
+  end
+
+  def municipal
+    object.street.ward.municipal
+  end
+
+  def roles
+    object.roles
   end
 end
