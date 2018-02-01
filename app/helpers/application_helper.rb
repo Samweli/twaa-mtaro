d1c45996e94fabd11cda7b4ed72e9a867b50c3f5
Mloweedgar <mloweedgar0@gmail.com>
2018-02-01 15:36:21 +0300

    creating and using helpers to restring user actions on need help details

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,5 +7,25 @@ module ApplicationHelper
     value ? t("notices.no_value") : t("notices.yes_value")
   end
 
+  # checks if street of current user
+  # is the same as drains street
+  def my_street(drain)
+    if user_signed_in?
+      return true if drain.streets.include? current_user.street
+    else
+      false
+    end
+  end
+
+  # checks if current user is the street leader
+  # of the drain's street
+  def iam_street_leader(drain)
+    if my_street(drain)
+      current_user.has_role(2)
+    else
+      false
+    end
+  end
+
 end
 
--- app/helpers/need_helps_helper.rb
+++ app/helpers/need_helps_helper.rb
@@ -1,2 +1,11 @@
 module NeedHelpsHelper
+  # checks if current user is the one
+  # who created the need help request
+  def my_need_help_request(need_help)
+    if iam_street_leader(need_help.drain)
+      need_help.user == current_user
+    else
+      false
+    end
+  end
 end
