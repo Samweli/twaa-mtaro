300d7e2c2a8b821e333a342a86e85325f9db0778
Samweli <smwltwesa6@gmail.com>
2018-02-20 22:30:30 +0300

    created test stubs helper

--- test/functional/drain_claims_controller_test.rb
+++ test/functional/drain_claims_controller_test.rb
@@ -2,12 +2,13 @@ require 'test_helper'
 
 class DrainClaimsControllerTest < ActionController::TestCase
   include Devise::TestHelpers
+  include TestStubHelper
   setup do
     request.env["devise.mapping"] = Devise.mappings[:user]
     @user = users(:test)
     osm_geocode_api = "http://nominatim.openstreetmap.org/search"
     twillio_api = "https://api.twilio.com"
-    stubs
+    drain_claims_stubs
     
  #    stub_request(:get, osm_geocode_api)
 	#   .with(headers: {})
@@ -59,31 +60,6 @@ class DrainClaimsControllerTest < ActionController::TestCase
     get :destroy, {:id => 1}
     assert_response :redirect
     # assert_template "drain_claims/show"
-  end
-
-
-  def stubs
-    stub_request(:get, "http://nominatim.openstreetmap.org/search?addressdetails=1&format=json&polygon=0&q=Sheki%20Road,%20Dar%20es%20salaam").
-    with(  headers: {
-      'Accept'=>'*/*',
-      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-      'User-Agent'=>'Ruby'
-      }).
-    to_return(status: 200, body: "", headers: {})
-
-    stub_request(:post, "https://api.twilio.com/2010-04-01/Accounts/AC12f20bfa2f6630b53c40266939dab38e/Messages.json").
-    with(
-      body: {"Body"=>"You have been assigned drain with number 1 located at Sheki Road.", "From"=>"14256540807", "To"=>"0765299266"},
-      headers: {
-      'Accept'=>'application/json',
-      'Accept-Charset'=>'utf-8',
-      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-      'Authorization'=>'Basic QUMxMmYyMGJmYTJmNjYzMGI1M2M0MDI2NjkzOWRhYjM4ZTpjMmU0ZjEzODM1OGEzMDBmMDcwMzFjNzAyNmM5MzQxZg==',
-      'Content-Type'=>'application/x-www-form-urlencoded',
-      'User-Agent'=>'twilio-ruby/4.13.0 (ruby/x86_64-linux 2.2.3-p173)'
-      }).
-    to_return(status: 200, body: "", headers: {})
-  
   end
 
 end
--- test/functional/drains_controller_test.rb
+++ test/functional/drains_controller_test.rb
@@ -2,11 +2,12 @@ require 'test_helper'
 
 class DrainsControllerTest < ActionController::TestCase
   include Devise::TestHelpers
+  include TestStubHelper
   setup do
     request.env["devise.mapping"] = Devise.mappings[:user]
     @user = users(:sleader)
     @drain = mitaro_dar(:one)
-    stubs
+    drain_stubs
   end
 
   test 'should get index' do
@@ -32,32 +33,5 @@ class DrainsControllerTest < ActionController::TestCase
     get :set_flood_prone , {:drain_id => 1}
     assert_response :success
   end
-
-
-  def stubs
-    stub_request(:get, "http://nominatim.openstreetmap.org/search?addressdetails=1&format=json&polygon=0&q=,%20").
-    with(  headers: {
-      'Accept'=>'*/*',
-      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-      'User-Agent'=>'Ruby'
-      }).
-    to_return(status: 200, body: "", headers: {})
-
-     stub_request(:post, "https://api.twilio.com/2010-04-01/Accounts/AC12f20bfa2f6630b53c40266939dab38e/Messages.json").
-    with(
-      body: {"Body"=>"You have successfully updated drain with number 1 to flood prone.", "From"=>"14256540807", "To"=>"0655899266"},
-      headers: {
-      'Accept'=>'application/json',
-      'Accept-Charset'=>'utf-8',
-      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-      'Authorization'=>'Basic QUMxMmYyMGJmYTJmNjYzMGI1M2M0MDI2NjkzOWRhYjM4ZTpjMmU0ZjEzODM1OGEzMDBmMDcwMzFjNzAyNmM5MzQxZg==',
-      'Content-Type'=>'application/x-www-form-urlencoded',
-      'User-Agent'=>'twilio-ruby/4.13.0 (ruby/x86_64-linux 2.2.3-p173)'
-      }).
-    to_return(status: 200, body: "", headers: {})
-
-
-  end
-
  
 end
