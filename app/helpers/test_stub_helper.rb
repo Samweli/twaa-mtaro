3fa6ab9ad7ac59dfbcd9c93a9f2e6b9c6ced45e5
Samweli <smwltwesa6@gmail.com>
2018-02-21 10:53:42 +0300

    added travis stubs for drain claims controller tests

--- app/helpers/test_stub_helper.rb
+++ app/helpers/test_stub_helper.rb
@@ -1,19 +1,6 @@
 module TestStubHelper
   
   def drain_stubs
-    stub_request(:post, "https://api.twilio.com/2010-04-01/Accounts/67aa405/Messages.json").
-      with(
-        body: {"Body"=>"You have successfully updated drain with number 1 to flood prone.", "From"=>"255789454545", "To"=>"0655899266"},
-        headers: {
-        'Accept'=>'application/json',
-        'Accept-Charset'=>'utf-8',
-        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-        'Authorization'=>'Basic NjdhYTQwNTo5YjMxMWMw',
-        'Content-Type'=>'application/x-www-form-urlencoded',
-        'User-Agent'=>'twilio-ruby/4.13.0 (ruby/x86_64-linux 2.2.3-p173)'
-        }).
-      to_return(status: 200, body: "", headers: {})
-
       stub_request(:get, "http://nominatim.openstreetmap.org/search?addressdetails=1&format=json&polygon=0&q=,%20").
       with(  headers: {
         'Accept'=>'*/*',
@@ -30,6 +17,20 @@ module TestStubHelper
         'Accept-Charset'=>'utf-8',
         'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
         'Authorization'=>'Basic QUMxMmYyMGJmYTJmNjYzMGI1M2M0MDI2NjkzOWRhYjM4ZTpjMmU0ZjEzODM1OGEzMDBmMDcwMzFjNzAyNmM5MzQxZg==',
+        'Content-Type'=>'application/x-www-form-urlencoded',
+        'User-Agent'=>'twilio-ruby/4.13.0 (ruby/x86_64-linux 2.2.3-p173)'
+        }).
+      to_return(status: 200, body: "", headers: {})
+
+      # Travis stubs
+      stub_request(:post, "https://api.twilio.com/2010-04-01/Accounts/67aa405/Messages.json").
+      with(
+        body: {"Body"=>"You have successfully updated drain with number 1 to flood prone.", "From"=>"255789454545", "To"=>"0655899266"},
+        headers: {
+        'Accept'=>'application/json',
+        'Accept-Charset'=>'utf-8',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'Authorization'=>'Basic NjdhYTQwNTo5YjMxMWMw',
         'Content-Type'=>'application/x-www-form-urlencoded',
         'User-Agent'=>'twilio-ruby/4.13.0 (ruby/x86_64-linux 2.2.3-p173)'
         }).
@@ -54,6 +55,21 @@ module TestStubHelper
         'Accept-Charset'=>'utf-8',
         'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
         'Authorization'=>'Basic QUMxMmYyMGJmYTJmNjYzMGI1M2M0MDI2NjkzOWRhYjM4ZTpjMmU0ZjEzODM1OGEzMDBmMDcwMzFjNzAyNmM5MzQxZg==',
+        'Content-Type'=>'application/x-www-form-urlencoded',
+        'User-Agent'=>'twilio-ruby/4.13.0 (ruby/x86_64-linux 2.2.3-p173)'
+        }).
+      to_return(status: 200, body: "", headers: {})
+
+      # travis stubs
+
+      stub_request(:post, "https://api.twilio.com/2010-04-01/Accounts/67aa405/Messages.json").
+      with(
+        body: {"Body"=>"You have been assigned drain with number 1 located at Sheki Road.", "From"=>"255789454545", "To"=>"0765299266"},
+        headers: {
+        'Accept'=>'application/json',
+        'Accept-Charset'=>'utf-8',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'Authorization'=>'Basic NjdhYTQwNTo5YjMxMWMw',
         'Content-Type'=>'application/x-www-form-urlencoded',
         'User-Agent'=>'twilio-ruby/4.13.0 (ruby/x86_64-linux 2.2.3-p173)'
         }).
